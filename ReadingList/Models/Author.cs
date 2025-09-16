82de6734ca007d1a23904d53dee14a9f451c2a07
Halfdan Sand Christensen <halfdansc@proton.me>
2025-09-16 19:27:39 +0200

    small clean up after intial structure

--- ReadingList/Models/Author.cs
+++ ReadingList/Models/Author.cs
@@ -3,6 +3,6 @@ namespace ReadingList.Models;
 public class Author
 {
     public int Id { get; set; }
-    public string FullName { get; set; }
+    public required string FullName { get; set; }
     public List<Book> books { get; set; } = new List<Book>();
 }
